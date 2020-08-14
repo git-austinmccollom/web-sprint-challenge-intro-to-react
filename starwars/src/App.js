9415b8eae06f799fe42d8dc083aff100702c8607
git-austinmccollom <austinmccollom@gmail.com>
2020-08-14 11:35:02 -0500

    showing component for each character object in characters state array

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import Character from './components/Character'
 
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characters, setCharacters] = useState('hello');
+  const [characters, setCharacters] = useState([]);
   console.log(characters);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -28,6 +29,11 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">characters</h1>
+      {
+        characters.map( ch => {
+          return <Character />
+        })
+      }
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,9 @@
-// Write your Character component here
+import React from 'react'
+// import styled from 'styled-components'
+
+export default function Character() {
+
+    return(
+        <p>hello?</p>
+    )
+}
